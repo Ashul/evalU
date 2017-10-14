6d90b9fb211a1bf36072b0d7245e7e516c04ed4b
Ashul <anshulupsc@gmail.com>
2017-10-14 19:39:58 +0530

    Update reset.js

--- client/controllers/reset.js
+++ client/controllers/reset.js
@@ -1,16 +1,24 @@
 angular.module('MyApp')
-  .controller('ResetCtrl', function($scope, Account) {
-    $scope.resetPassword = function() {
-      Account.resetPassword($scope.user)
+  .controller('ResetCtrl', ['$http','Account','$routeParams',function($http, Account, $routeParams) {
+    var main = this;
+    main.resetPassword = function() {
+
+        var myData = {
+            password        :       main.password,
+
+        }
+        main.token = $routeParams.token;    
+        $http.post('/reset/'+main.token, myData)
         .then(function(response) {
-          $scope.messages = {
+          main.messages = {
             success: [response.data]
           };
         })
-        .catch(function(response) {
-          $scope.messages = {
+         .catch(function(response) {
+          main.messages = {
             error: Array.isArray(response.data) ? response.data : [response.data]
           };
-        });
+        })
     }
-  });
+
+  }]);
